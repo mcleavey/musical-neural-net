9153527e24cdc163a5cd4be7b68992adf2a309e6
Christine Payne <mcleavey@gmail.com>
2018-08-30 08:08:26 -0700

    Composer Classifier comments

--- composer_classifier.py
+++ composer_classifier.py
@@ -4,33 +4,43 @@ import dill as pickle
 import argparse
 
 
-def train(model, training, use_pretrain, epochs, bs):    
+def train(model_to_load, training, use_pretrain, epochs, bs):    
     PATHS=create_paths()
-    TEXT=pickle.load(open(f'{PATHS["generator"]}/{model}_text.pkl','rb'))    
-    params=pickle.load(open(f'{PATHS["generator"]}/{model}_params.pkl','rb'))
+    
+    # TEXT holds information about the vocab to index mapping
+    # params holds the hyperparameters of the model (embedding size, number layers, etc)
+    TEXT=pickle.load(open(f'{PATHS["generator"]}/{model_to_load}_text.pkl','rb'))    
+    params=pickle.load(open(f'{PATHS["generator"]}/{model_to_load}_params.pkl','rb'))
     
     print("Loading dataset")
     MUSIC_LABEL = data.Field(sequential=False)
     splits = ComposerDataset.splits(TEXT, MUSIC_LABEL, PATHS["composer_data"])
-    md = TextData.from_splits(PATHS["data"], splits, bs)
+    music_data = TextData.from_splits(PATHS["data"], splits, bs)
 
     print("Initializing model")
-    opt_fn = partial(optim.Adam, betas=(0.7, 0.99))
-    m = md.get_model(opt_fn, 1500, bptt=params["bptt"], emb_sz=params["em_sz"], n_hid=params["nh"], n_layers=params["nl"], 
-           dropout=0.1, dropouti=0.4, wdrop=0.5, dropoute=0.05, dropouth=0.3)
-    m.reg_fn = partial(seq2seq_reg, alpha=2, beta=1)
-    m.clip=25.
+    optimizer_function = partial(optim.Adam, betas=(0.7, 0.99))  # Adam optimizer with lower momentum
+    
+    model = music_data.get_model(optimizer_function, 1500, bptt=params["bptt"], emb_sz=params["em_sz"], 
+                                 n_hid=params["nh"], n_layers=params["nl"], dropout=0.1, dropouti=0.4,
+                                 wdrop=0.5, dropoute=0.05, dropouth=0.3)  # Dropout defaults from AWD paper
+    
+    model.reg_fn = partial(seq2seq_reg, alpha=2, beta=1)         # Regularization (from FastAI library)
+    model.clip=25.                                               # Clipping gradients
     
     if use_pretrain:
         raise NotImplementedError
         
+    # Training uses cyclical learning rates. These are the initial weights for 
+    # each cycle. The model is saved as _light after the first cycle, as _med after
+    # the second cycle, etc. Each cycle is "epochs" long
+    
     lrs=[3e-3, 3e-4, 3e-6, 3e-8]
     trainings=["_light.pth", "_med.pth", "_full.pth", "_extra.pth"] 
-    save_names=[model+b for b in trainings]
+    save_names=[model_to_load+b for b in trainings]
     save_names=[PATHS["composer"]/s for s in save_names]
         
     for i in range(len(lrs)):
-        train_and_save(m, lrs[i], epochs, save_names[i], metrics=[accuracy])
+        train_and_save(model, lrs[i], epochs, save_names[i], metrics=[accuracy])
 
 
 
